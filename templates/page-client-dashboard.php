d98a73f50dd5ee50eb2462adb0adc3d07c72e707
Blitzshadow <90090472+Blitzshadow@users.noreply.github.com>
2025-09-10 07:48:08 +0200

    Poprawa ścieżek do assets: /wp-content/plugins/new-panel-master/assets/

--- templates/page-client-dashboard.php
+++ templates/page-client-dashboard.php
@@ -9,11 +9,11 @@ if (!defined('ABSPATH')) exit;
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Panel Klienta WPaaS</title>
-	<link rel="stylesheet" href="/wp-content/plugins/new-panel/assets/tailwind.min.css">
+	<link rel="stylesheet" href="/wp-content/plugins/new-panel-master/assets/tailwind.min.css">
 </head>
 <body class="bg-gray-900 text-gray-100 min-h-screen">
 	<?php include __DIR__.'/dashboard.php'; ?>
-	<script src="/wp-content/plugins/new-panel/assets/alpine.min.js"></script>
-	<script src="/wp-content/plugins/new-panel/assets/dashboard.js"></script>
+	<script src="/wp-content/plugins/new-panel-master/assets/alpine.min.js"></script>
+	<script src="/wp-content/plugins/new-panel-master/assets/dashboard.js"></script>
 </body>
 </html>
